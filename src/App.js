7956d0523f236fc0a84dfd2721eac038e812d852
rifqi <rifqhiyr@gmail.com>
2019-12-03 15:52:56 +0700

    [rifqi] - merge master

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
+import Blog from "./components/pages/Blog";
+import About from "./components/pages/About";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/blog" exact component={Blog} />
+        <Route path="/about" exact component={About} />
+      </Router>
+    );
+  }
 }
 
 export default App;
